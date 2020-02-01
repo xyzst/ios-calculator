d8e16ba8a090c1ef870f7b9029c2bb4bb8dc9a4f
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-01 17:55:25 -0600

    Initialize Calculator Project
    Initial commit for an Xcode/Swift project. Include static assets (0-9, +, -, ...). There's no logic at the moment to perform calculation.
    
    Related to: none

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Darren Rambaud on 02/01/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
